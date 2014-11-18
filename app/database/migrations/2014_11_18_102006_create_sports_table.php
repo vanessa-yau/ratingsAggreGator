8439fac9a57d2ec4062915b13727d832694bda68
Ryan Gibbs <ryangibbs11@msn.com>
2014-11-18 10:21:49 +0000

    sports table migration and added char limits to string fields.

--- app/database/migrations/2014_11_18_102006_create_sports_table.php
+++ app/database/migrations/2014_11_18_102006_create_sports_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePlayersTable extends Migration {
+class CreateSportsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,17 +12,17 @@ class CreatePlayersTable extends Migration {
 	 */
 	public function up()
 	{
-	    Schema::create('players', function($table)
+	    Schema::create('ratings', function($table)
 	    {
 	        $table->increments('id');
-	        $table->string('name', 100);
+	        $table->string('name', 100)
 	        $table->timestamps();
 	    });
 	}
 
 	public function down()
 	{
-	    Schema::drop('players');
+	    Schema::drop('ratings');
 	}
 
 }
