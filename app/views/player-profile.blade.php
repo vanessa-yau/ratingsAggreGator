f042d9985887164151b4defc2b6be7d64b666a7a
djdavies <jdavies.thfc@gmail.com>
2014-11-19 11:44:04 +0000

    Tidied main player rating view

--- app/views/player-profile.blade.php
+++ app/views/player-profile.blade.php
@@ -1,159 +1,134 @@
 @extends('master')
 
 @section('style')
-	{{ HTML::style("/css/player-profile.css") }}
+  {{ HTML::style("/css/player-profile.css") }}
 @stop
 
 @section('content')
-	<div class="container">
-		<div class="row">
-			<div class="col-md-3">
-				{{ HTML::image(
-		  		'/images/profile_images/gerrard_steven.jpg',	
-		  		'image not found', ['class' => 'profile-img']) 
-		  	}}
-			</div>
-		</div>
+  <div class="row well">
+    <div class="col-md-3">
+      {{ HTML::image(
+        '/images/profile_images/gerrard_steven.jpg',  
+        'image not found', ['class' => 'profile-img']) 
+      }}
+    </div>
+  </div>
 
-		<!-- Aggregate ratings info for player -->
-		<div class="row">
-			<div class="col-md-12">
-				<!-- BLAH -->
-				<p>some aggregate content</p>
-			</div>
-		</div>
+  <!-- ratings form -->
+  <div class="row well">
+    <div class="col-md-12">
+      
+      <form 
+          class="form-horizontal" 
+          id="rate-player-form"
+          role="form"
+          method="POST" 
+          action="{{ URL::route('players.store') }}"
+          novalidate
+        >
 
-		<!-- ratings form -->
-		<div class="row well">
-			<div class="col-md-12">
-				<h4>RATE THIS PLAYER</h4>
-				
-				<form 
-		      class="form-horizontal" 
-		      id="rate-player-form"
-		      role="form"
-		      method="POST" 
-		      action="{{ URL::route('players.store') }}"
-		      novalidate
-		    >
-		    	<div class="row">
-		    		<input type="hidden" name="player_id" value="1">
-		    		
-		    		<h4>Match info</h4>
-						
-						<!-- enter/select match -->
-						<div class="form-group match">
-			        <!-- select teams -->
-			        <div class="col-sm-10">
-			        	<input 
-			        		id="team_1" 
-			        		type="text" 
-			        		name="team_1" 
-			        		placeholder="Enter a team" 
-			        		class="pull-right"
-			        	>
-								
-								<p> vs </p>
-								
-								<input 
-			        		id="team_2" 
-			        		type="text" 
-			        		name="team_2" 
-			        		placeholder="Enter a team" 
-			        		class="pull-left"
-			        	>
-							</div>
+        <input type="hidden" name="player_id" value="1">
+          
+         
+        <!-- row for team y vs team x -->  
+        <div class="row">    
+          <h4>Match info</h4>
+          <!-- enter/select match -->
+          <div class="form-group match">
+            <!-- select teams -->
+            <div class="col-sm-10">
+              <input 
+                id="team_1" 
+                type="text" 
+                name="team_1" 
+                placeholder="Enter home team" 
+              >
 
-							<!-- select match date -->
-							<input 
-								id="match_date"
-								name="match_date"
-								type="datetime"
-								placeholder="Enter a date"
-								class="form-control datepicker"
-							>
-			      </div>
-		    	</div>
+              vs
 
-					<div class="row skills">
-						<!-- different attributes to rate a player on -->
-			    	@foreach( $attributes as $attr )
-				    	<div class="form-group">
-				        <label for="{{{ $attr }}}" class="col-sm-2 control-label">{{{ $attr }}}</label>
-				        
-				        <div class="col-sm-10">
-								 	<select name="{{{ $attr }}}" id="{{ $attr }}">
-								 		<option value="6">Please select</option>
-								 		<option value="1">Rubbish!</option>
-								 		<option value="2">Poor</option>
-								 		<option value="3">Average</option>
-								 		<option value="4">Good</option>
-								 		<option value="5">Sublime!</option>
-								 	</select>
-								</div>
+              <input 
+                id="team_2" 
+                type="text" 
+                name="team_2" 
+                placeholder="Enter away team" 
+              >
+        
 
-				      </div>
-			      @endforeach
-					</div>
 
-					<input type="hidden" id="player_id" value="1">
+            <!-- select match date,
+            to be replaced by interactive calendar 
+            -->
+              <input 
+                id="match_date"
+                name="match_date"
+                type="datetime"
+                placeholder="Enter a date: dd/mm/yyyy"
+              >
+            </div> <!-- end col -->
+          </div> <!-- end form group -->
+        </div> <!-- end row div -->
 
-					<div class="form-group">
-		        <div class="col-sm-12">
-		          <input 
-		          	id="submit-ratings-btn" 
-		          	type="submit" 
-		          	value="Submit My Ratings" 
-		          	class="btn login-btn btn-primary pull-right"
-		          >
-		        </div>
-		      </div>
+        <div class="row skills">
+          <!-- different attributes to rate a player on -->
+          @foreach( $attributes as $attr )
+            <div class="form-group">
+              <label for="{{{ $attr }}}" class="col-sm-2 control-label">{{{ $attr }}}</label>
+              
+              <div class="col-sm-10">
+                <select name="{{{ $attr }}}" id="{{ $attr }}">
+                  <option value="6">Please select</option>
+                  <option value="1">Rubbish!</option>
+                  <option value="2">Poor</option>
+                  <option value="3">Average</option>
+                  <option value="4">Good</option>
+                  <option value="5">Sublime!</option>
+                </select>
+              </div>
 
-		    </form>
-			</div>
-		</div>
+            </div>
+          @endforeach
+        </div> <!-- end row skills row -->
 
-		<!-- charts -->
-		<div class="row">
-			<div class="col-md-12">
-				<!-- some very cool charts here -->
-				<p>some cool charts</p>
-			</div>
-		</div>
-	</div>
+        <input type="hidden" id="player_id" value="1">
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input 
+              id="submit-ratings-btn" 
+              type="submit" 
+              value="Submit My Ratings" 
+              class="btn login-btn btn-primary pull-right"
+            >
+          </div>
+        </div>
+
+      </form>
+    </div>
+  </div> <!-- end row well div -->
 @stop
 
 @section('js')
-	<script src="/js/jquery-ui.js"></script>
-	<script>
-		$(function(){
-			$('.dropdown-menu li').click(function(e){
-				e.preventDefault();
-				$this = $(e.target);
-				$this
-					.parents('.btn-group')
-					.find('.selected-rating')
-					.text($this.text());
-			});
-
-			$('#submit-ratings-btn').click(function(e){
-				e.preventDefault();
-				if( $('.skills').find('select').val() == "6" ){
-					alert('Please select a rating for all the categories.');
-				}
-				else{
-					$.ajax({
-						type: "POST",
-						url: $('#rate-player-form').attr('action')
-						success: function(json){
-							alert('Thanks for rating!');
-						},
-						error: function(e){
-							console.log(e);
-						}
-					});
-				}
-			});
-		});
-	</script>
+  <script src="/js/jquery-ui.js"></script>
+  <script>
+    $(function(){
+      // when buttton is clicked
+      $('#submit-ratings-btn').click(function(e){
+        e.preventDefault();
+        if( $('.skills').find('select').val() == "6" ){
+          alert('Please select a rating for all the categories.');
+        }
+        else{
+          $.ajax({
+            type: "POST",
+            url: $('#rate-player-form').attr('action')
+            success: function(json){
+              alert('Thanks for rating!');
+            },
+            error: function(e){
+              console.log(e);
+            }
+          });
+        }
+      });
+    });
+  </script>
 @stop
