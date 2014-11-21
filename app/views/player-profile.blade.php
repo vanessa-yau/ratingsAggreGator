4ad62008f6a9abafa1231fb94c1e6eb35a6ee75a
Ryan Gibbs <ryangibbs11@msn.com>
2014-11-21 15:58:27 +0000

    added chart js to public and supplied sample chart in player-profile.blade.php

--- app/views/player-profile.blade.php
+++ app/views/player-profile.blade.php
@@ -132,6 +132,9 @@
             </form>
         </div>
     </div> <!-- end row well div -->
+
+    <canvas id="myChart" width="400" height="400"></canvas>
+
 @stop
 
 @section('js')
@@ -141,6 +144,50 @@
     $('#response-message').hide();
 
     $(function(){
+      
+        var data = [
+            {
+                value: 300,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 50,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Green"
+            },
+            {
+                value: 100,
+                color: "#FDB45C",
+                highlight: "#FFC870",
+                label: "Yellow"
+            },
+            {
+                value: 40,
+                color: "#949FB1",
+                highlight: "#A8B3C5",
+                label: "Grey"
+            },
+            {
+                value: 120,
+                color: "#4D5360",
+                highlight: "#616774",
+                label: "Dark Grey"
+            }
+
+        ];
+
+        // <!> this part needs to go before the new chart statement below <!>
+
+        // Get the context of the canvas element we want to select
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myNewChart = new Chart(ctx).PolarArea(data);
+        
+        // creates the new chart on the canvas supplied in the ctx.
+        new Chart(ctx).PolarArea(data);
+
         // when page is loaded, change colours of skill rating boxes as apt.
         function colourStatPanels() {
             var stats = $('.stat-panel');
