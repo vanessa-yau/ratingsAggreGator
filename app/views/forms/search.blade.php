9219214d6a054a710ea828619bf4fa7bb290e095
sam reilly <samreilly100@hotmail.co.uk>
2014-11-24 16:55:24 +0000

    Fixed  an accidental bug with the search feature

--- app/views/forms/search.blade.php
+++ app/views/forms/search.blade.php
@@ -1,8 +1,8 @@
-<form action ="{{ URL::route('players.search') }}" class="navbar-form navbar-left" role="search" style="max-width:100%" id="search-form" method="get">
+<form action ="{{ URL::route('players.search') }}" class="navbar-form navbar-left search-form" role="search" style="max-width:100%" name="search-form" method="get">
     <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search Players by Name" id="search-box" name="search-box">
+        <input type="text" class="form-control" placeholder="Search Players by Name" name="search-box">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="submit" id="search-button">&nbsp;<span class="glyphicon glyphicon-search"></span>&nbsp;</button>
+            <button class="btn btn-default" type="submit">&nbsp;<span class="glyphicon glyphicon-search"></span>&nbsp;</button>
         </span>
     </div>
 </form>
