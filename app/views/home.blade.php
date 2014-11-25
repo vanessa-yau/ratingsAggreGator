95fdac8bbda2b12cadc9b2673bfd9ff47b0fcd8d
Ryan Gibbs <ryangibbs11@msn.com>
2014-11-25 14:13:53 +0000

    resolved conflict

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -13,22 +13,10 @@
 
 	@foreach($players as $player)
 		<div class="col-xs-12 col-sm-6 col-lg-4">
-<<<<<<< HEAD
-			<a href="/players/{{ $player->id }}">
+			<a href="{{ $player->url }}">
 				<span class="name">{{ $player->name }}</span>
 				<img class="thumbnail" src="{{ $player->image_url }}" alt="Profile Image">
 			</a>
-=======
-			<span class="name">{{ $player->name }}</span>
-
-			@if( $player->image_url )
-				<a href="{{ $player->url }}">
-					<img class="thumbnail" src="{{ $player->image_url }}" alt="Profile Image">
-				</a>
-			@else
-				<img class="thumbnail" src="/images/profile_images/placeholder.png" alt="Profile Image">
-			@endif
->>>>>>> f5fa8f6411603fee9db534ae6ce72dcba28cf53c
 		</div>
 	@endforeach
 
