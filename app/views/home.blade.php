192ac19d54caf18e62785d93b799561954b3039a
sam reilly <samreilly100@hotmail.co.uk>
2014-11-25 10:03:16 +0000

    Changed home page so player pictures can be clicked to bring users to their profile

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,13 +10,17 @@
 		<h3>Most Rated - Today's most commonly rated players</h3>
 	</div>
 	<div align="center">
+
 	@foreach($players as $player)
 		<div class="col-xs-12 col-sm-6 col-lg-4">
 			<span class="name">{{ $player-> name }}</span>
-			<img class="thumbnail" src="{{{ $player->image_url }}}" alt="Image of player">
-			
+
+			<a href="{{ $player->url }}">
+				<img class="thumbnail" src="{{{ $player->image_url }}}" alt="Image of player">
+			</a>
 
 		</div>
 	@endforeach
+
 	</div>
 @stop
