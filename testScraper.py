56d61f5963d2ab7711666beff55ae7552c99ffa9
djdavies <jdavies.thfc@gmail.com>
2014-11-25 17:09:08 +0000

    manually added more to players.json for seeding purposes

--- testScraper.py
+++ testScraper.py
@@ -5,9 +5,12 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-url = 'http://www.footballsquads.co.uk/eng/2014-2015/faprem/tottenha.htm'
-content = urllib2.urlopen(url).read()
-soup = BeautifulSoup(content)
+#url = 'http://www.footballsquads.co.uk/eng/2014-2015/faprem/tottenha.htm'
+#content = urllib2.urlopen(url).read()
+#soup = BeautifulSoup(content)
+
+r = requests.get("http://www.footballsquads.co.uk/eng/2014-2015/faprem/tottenha.htm")
+
 
 # prints out everything, .encode because it messes up on a unicode char
 #print soup.prettify().encode(sys.stdout.encoding, errors='replace')
@@ -15,12 +18,15 @@ soup = BeautifulSoup(content)
 # print out the title
 #print soup.title.string
 
+tottenhamPlayers = soup.find_all("td")
+
 # print out all the tds
-for link in soup.find_all('td'):
-	print(link)
+for links in soup.find_all('td'):
+	print(links)
 
 # replaces unicode with '?'...
-print soup.get_text()
+#print soup.get_text().encode(sys.stdout.encoding, errors='replace')
 
 # this prints fine
 #print u"Stöcker".encode(sys.stdout.encoding, errors='replace')
+
