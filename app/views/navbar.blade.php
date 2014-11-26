e3c2b871ea3b605872a3f5e9853b517ba75bff83
Vanessa Yau <vyau@hotmail.co.uk>
2014-11-26 16:11:50 +0000

    drop down nav menu - reordered elements on page

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -2,16 +2,16 @@
     <div class="container">
         <!-- logo and shrink menu icon -->
         <div class="navbar-header">
+            <a class="navbar-brand" href="{{ URL::to('/') }}">
+                <img src="/images/gator.jpg" alt="..." class="logo">
+                ratingator
+            </a>
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ URL::to('/') }}">
-                <img src="/images/gator.jpg" alt="..." class="logo">
-                ratingator
-            </a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
