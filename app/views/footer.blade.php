3c523daf6a6bfd57b83498946c5a973eaaa4ee34
Vanessa Yau <vyau@hotmail.co.uk>
2014-11-27 12:31:20 +0000

    navbar (and css) added to sticky footer, uservoice feedback script added. obsolete file deleted

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,5 +1,24 @@
+{{ HTML::style("/css/footer.css") }}
+
 <footer class="footer">
     <div class="container">
-        <p class="text-muted">Place sticky footer content here.</p>
+        <!-- Static navbar -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <p class="navbar-brand text-muted">ratingator</p>
+                <!-- <a class="navbar-brand" href="#">Project name</a> -->
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </nav>
     </div>
 </footer>
