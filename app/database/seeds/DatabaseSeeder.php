5d1d219012299af3f30770a40363f7dcd5cb36bb
djdavies <jdavies.thfc@gmail.com>
2014-12-01 16:34:02 +0000

    scraped and seeded spanish La Liga players/teams

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -18,6 +18,6 @@ class DatabaseSeeder extends Seeder {
         $this->call('ProfileSkillTableSeeder');
         $this->call('TeamTableSeeder');
         $this->call('PlayerEnglishPremierLeagueSeeder');
+        $this->call('PlayerSpanishLaLigaSeeder');
     }
-
 }
