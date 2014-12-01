3dba354aeb85fba3d76c798be609fec08f0909bd
djdavies <jdavies.thfc@gmail.com>
2014-12-01 10:50:41 +0000

    removed seeder code from routes. added class into main DB seeder. please run db:seed locally

--- app/routes.php
+++ app/routes.php
@@ -11,35 +11,6 @@
 |
 */
 
-// this function should be moved to seeds when possible
-Route::get('tinker', function () {
-    $raw = File::get('simple.json');
-    $json = json_decode($raw, true);
-
-    if ($json) {
-        foreach ($json['teams'] as $team) {
-
-            // create team model
-            if (! Team::whereName($team['name'])->count() )
-                $teamModel = Team::create(['name' => $team['name']]);
-
-
-            echo $team['name'] . "<br>";
-            foreach ($team['players'] as $player) {
-
-                $values = array_only($player, ['name']);
-                if ( array_key_exists('name', $values) && $values['name']) {
-                    $playerModel = Player::create([
-                        'name' => $player['name'],
-                        'last_known_team' => $player['team']
-                    ]);
-
-                }
-            }
-        }
-    }
-});
-
 Route::get('/',  [
     'as' => 'home',
     'uses' => 'RatingController@mostPopular'
