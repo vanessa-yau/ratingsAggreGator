0f49828099022d8acd3f02f4f75d8d399b4f2bae
Vanessa Yau <vyau@hotmail.co.uk>
2014-12-03 13:19:35 +0000

    Added css styling to team-profile page

--- app/views/team-profile.blade.php
+++ app/views/team-profile.blade.php
@@ -1,9 +1,28 @@
 @extends('master')
 
+@section('style')
+    <style>
+        .team-badge {
+            max-width: 100px;
+            max-height: 100px;
+        }
+        .team-info {
+            border-left-width: 1px;
+            border-left-style: solid;
+            border-left-color: lightgrey;
+        }
+    </style>
+@stop
+
 @section('content')
+    <!-- general team info -->
     <div class="row">
-        <div class="col-sm-12">
-            <p>{{ $team->name }}</p>
+        <div class="col-sm-2">
+            <img class="team-badge pull-right" src="/images/gator.jpg" alt="">
+        </div>
+        <div class="col-sm-10 team-info">
+            <h1>{{ $team->name }}</h1>
+            <h2>Currently in: {{{ $team->last_known_league_id }}}</h2>
         </div>
     </div>
 @stop
