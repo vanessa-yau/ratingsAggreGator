0f8b3985b1155e47c99b0f908c347067392b56f1
Vanessa Yau <vyau@hotmail.co.uk>
2014-12-03 11:21:15 +0000

    added leagues view

--- app/views/leagues.blade.php
+++ app/views/leagues.blade.php
@@ -1,5 +1,18 @@
 @extends('master')
 
+@section('style')
+    <style>
+        .league-teams {
+            display: none;
+        }
+
+        img {
+            max-width: 50px;
+            max-height: 50px;
+        }
+    </style>
+@stop
+
 @section('content')
     <div class="leagues">
         <h2>Leagues</h2>
@@ -7,18 +20,25 @@
             @foreach( $leagues as $league )
                 <div class="row">
                     <div class="col-sm-12">
-                        <a href="#">
-                            <h3 class="league">{{{ $league->name }}}</h3>
-                        </a>
-                        <div class="row league-teams">
-                            @foreach( $league->teams as $team )
-                                <div class="col-xs-12 col-sm-6 col-lg-4">
-                                    <p>{{ $team->name }}</p>
-                                </div> 
-                            @endforeach
-                        </div> <!-- close row -->
+                        <div class="panel panel-default" data-id="{{{ $league->id }}}">
+                            <div class="panel-heading league">
+                                <img src="/images/leagues/englishpremier.jpg" alt="...">
+                                {{{ $league->name }}}
+                                <i class="glyphicon glyphicon-chevron-down pull-right"></i>
+                            </div>
+                            <div class="panel-body league-teams">
+                                <div class="row">
+                                    @foreach( $league->teams as $team )
+                                        <div class="col-xs-12 col-sm-6 col-lg-4">
+                                            <p>{{ $team->name }}</p>
+                                        </div>
+                                    @endforeach
+                                </div> <!-- close row -->
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             @endforeach
         @endif
     </div>
@@ -27,11 +47,24 @@
 @section('js')
     <script>
         $(function(){
-            $('.league-teams').hide();
-
             $( '.league' ).click(function(e){
+                var $this = $(e.target);
                 e.preventDefault();
-                $(this).find('league-teams').show();
+                var isHidden = $this.siblings().is( ":hidden" );
+                if( isHidden ){
+                    $this.siblings().slideDown(400, function() {
+                        $this.find('i').removeClass('glyphicon-chevron-down')
+                            .addClass('glyphicon-chevron-up');
+                        }
+                    );
+                }
+                else{
+                    $this.siblings().slideUp(400, function() {
+                        $this.find('i').removeClass('glyphicon-chevron-up')
+                            .addClass('glyphicon-chevron-down');
+                        }
+                    );
+                }
             });
         });
     </script>
