3b4fcce0fd8ec71e1fef221587406e62b6786dc1
Ryan Gibbs <ryangibbs11@msn.com>
2014-12-03 11:20:53 +0000

    added scrolling marquee to home page

--- app/views/magpie.blade.php
+++ app/views/magpie.blade.php
@@ -1,12 +1,14 @@
 <?php
-require_once(public_path().'/packages/magpierss/rss_fetch.inc');
+function getRss($sport){
+    require_once(public_path().'/packages/magpierss/rss_fetch.inc');
 
-$url = 'http://feeds.bbci.co.uk/sport/0/football/rss.xml?edition=uk';
-$rss = fetch_rss($url);
+    $url = "http://feeds.bbci.co.uk/sport/0/$sport/rss.xml?edition=uk";
+    $rss = fetch_rss($url);
 
-echo "Site: ", $rss->channel['title'], "<br>\n";
-foreach ($rss->items as $item ) {
-    $title = $item['title'];
-    $url   = $item['link'];
-    echo "<a href=$url>$title</a><br>\n";
+    // echo "Site: ", $rss->channel['title'], "<br>\n";
+    foreach ($rss->items as $item ) {
+        $title = $item['title'];
+        $url   = $item['link'];
+        echo "<a href=$url>$title</a>&nbsp&nbsp|&nbsp&nbsp";
+    }
 }
