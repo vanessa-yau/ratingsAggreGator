d54d643697a74d17117dc491c6424b27f847c9c5
sam reilly <samreilly100@hotmail.co.uk>
2014-12-05 12:17:31 +0000

    Added 3 new migrations for the twitter integration

---
+++ app/database/migrations/2014_12_05_121351_add_access_token_column_to users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAccessTokenColumnToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+		{
+		    $table->string('access_token', 255);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->dropColumn('access_token');
+		});
+	}
+
+}
