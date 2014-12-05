49f198e8e74d225ebdfd11fcca6cd1a0be0e13c5
sam reilly <samreilly100@hotmail.co.uk>
2014-12-05 16:57:24 +0000

    Another 2 required migrations added

---
+++ app/database/migrations/2014_12_05_153145_add_oauth_token_column_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOauthTokenColumnToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->string('oauth_token')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->dropColumn('oauth_token');
+		});
+	}
+
+}
