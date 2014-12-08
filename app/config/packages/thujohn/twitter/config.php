e8c982dce588b591a5ba720db04e9c3d16e49782
sam reilly <samreilly100@hotmail.co.uk>
2014-12-08 12:17:46 +0000

    Some changes to consumer keys, disabled login with twitter button

--- app/config/packages/thujohn/twitter/config.php
+++ app/config/packages/thujohn/twitter/config.php
@@ -11,8 +11,8 @@ return array(
 	'REQUEST_TOKEN_URL'   => 'oauth/request_token',
 	'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => 'oU616Feh1uFNle4e8lnz8pgfT',
-	'CONSUMER_SECRET'     => '6cLfvflVeCnj89ppOK7PQkAvZdUI0OYoSrDOBhCHeH8eEyi5xb',
+	'CONSUMER_KEY'        => '8iQBLVmS7bofjtSUeV5gGQg1k',
+	'CONSUMER_SECRET'     => 's8QMkVbczSgkfbOV4Re5eGrYGgWPE8DBm1dpo4AaIllucflxyw',
 	'ACCESS_TOKEN'        => '2893192415-ZsqwNwRYpIQevgH8Tt01G1Cv0HiVGK4p8gcZBQN',
 	'ACCESS_TOKEN_SECRET' => 'S5OEYZ1ENNIUfQVYzh9zK0uYnFF3Pc79DAlvrPYud8Xrr',
 );
