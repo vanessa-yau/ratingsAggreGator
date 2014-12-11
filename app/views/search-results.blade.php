dab23747d70d4660462561d8e992f623a7a4b7da
sam reilly <samreilly100@hotmail.co.uk>
2014-12-11 16:05:33 +0000

    Removed some unneeded code from search-results blade

--- app/views/search-results.blade.php
+++ app/views/search-results.blade.php
@@ -9,12 +9,14 @@ $html .= '</a>';
 $html .= '</li>';
 Clockwork::info($results);
 ?>
-
-@foreach($results as $result)
-	<!-- $name = preg_replace("/".$search_string."/i", "<b class='highlight'>".$search_string."</b>", $result['name']); -->
-	<li>{{{$result->name}}}</li>
-@endforeach
-
+	
+		<div class="results">
+			<ul>
+				@foreach($results as $result)
+					<li class="result"> <a href="/players/search/{{{$result->name}}}"> {{{$result->name}}} </a> </li>
+				@endforeach
+			</ul>
+		</div>
 @if(!$results->count())
 	<div class="row well message">
     	There are no results for your search.  Sorry about that.
