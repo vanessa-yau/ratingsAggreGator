d574dde66643058c5834306cd0739092d7b3fab3
sam reilly <samreilly100@hotmail.co.uk>
2014-12-15 15:17:54 +0000

    Some search bar fixes

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
+    <div class="container-fluid">
         <!-- logo and shrink menu icon -->
         <div class="navbar-header">
-            <a class="navbar-brand" href="{{ URL::to('/') }}">
-                <img src="/images/gator.jpg" alt="..." class="logo">
-                ratingator
+            <a href="{{ URL::to('/') }}">
+                <img class="site-logo" src="/images/gator.jpg" alt="...">
+                <span class="site-name">ratingator</span>
             </a>
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
@@ -16,36 +16,12 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <div class="visible-xs-inline">
+            <!-- <div class="visible-xs-inline"> -->
                 <ul class="collapsed-options nav navbar-nav">
-                    <a href="#">
-                        <li>@include('/forms/search')</li>
-                    </a>
-                    @if( Auth::user() )
-                        <a type="button" class="btn btn-block account-link" href="{{{ Auth::user()->url }}}">
-                            <li>
-                                My Account <span class="glyphicon glyphicon-cog"></span>
-                            </li>
-                        </a>
-                        <a href="{{ URL::route('users.logout') }}" class="login-button">
-                            <button class="btn btn-block" id="logout">
-                                <li>
-                                    Logout
-                                </li>
-                            </button>
-                        </a>
-                    @else
-                        <li>@include('/forms/login')</li>
-                    @endif
+                    <li>@include('/forms/search')</li>
+                    @include('/forms/login')
                 </ul>
-            </div>
-            <div class="hidden-xs">
-               @include("/forms/search")
-
-                <ul class="nav navbar-nav navbar-right">
-                    @include("/forms/login")
-                </ul><!-- /.navbar-right -->
-            </div>
+            <!-- </div> -->
         </div><!-- /.navbar-collapse -->
     </div>
 </nav>
