1d53119919beceae0ce80b9c7bc84eef3963c987
Ryan Gibbs <ryangibbs11@msn.com>
2014-12-16 13:51:21 +0000

    change file extension for league badges to support browser transparency

---
+++ app/database/migrations/2014_12_16_134437_changeFileExtensionLeagueBadges.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeFileExtensionLeagueBadges extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// change jpgs to pngs
+
+		// get all the leagues
+		$leagues = League::all();
+
+		foreach ($leagues as $league) {
+			$league->badge_image_url = str_replace(".jpg", ".png", $league->badge_image_url);
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// change jpgs to pngs
+
+		// get all the leagues
+		$leagues = League::all();
+
+		foreach ($leagues as $league) {
+			$league->badge_image_url = str_replace(".png", ".jpg", $league->badge_image_url);
+		}
+	}
+
+}
