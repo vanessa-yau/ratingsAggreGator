07e35b5f25f2d6b2e3870f7aa53d14d017bad10f
Vanessa Yau <vyau@hotmail.co.uk>
2014-12-19 11:50:04 +0000

    fixed the navbar pull right issue.

--- app/views/user-profile.blade.php
+++ app/views/user-profile.blade.php
@@ -5,38 +5,39 @@
 @stop
 
 @section('content')
-<!-- dynamically populated response message -->
-<div class="alert alert-dismissible" id="response-message" role="alert">
-  <button type="button" class="close" ><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-  <strong id="message-type"></strong><span id="message-text"></span>
-</div>
+    <!-- dynamically populated response message -->
+    <div class="alert alert-dismissible" id="response-message" role="alert">
+        <button type="button" class="close" ><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <strong id="message-type"></strong><span id="message-text"></span>
+    </div>
 
-<div class="row page-content">
-    <div>
-        <ul class="nav nav-tabs" role="tablist" id="tab-list">
-            <li class="active"><a href="#overview" role="tab" data-toggle="tab">Details <i class="fa fa-user"></i></a></li>
-            <li><a href="#ratings" role="tab" data-toggle="tab">Stats <i class="fa fa-bar-chart"></i></a></li>
-            @if(Auth::check() && Auth::id() == $user->id)
-                <!-- <li><a href="#messages" role="tab" data-toggle="tab">Messages <i class="fa fa-envelope"></i></a></li> -->
-                <li><a href="#settings" role="tab" data-toggle="tab">Settings <i class="fa fa-cog"></i></a></li>
-            @endif
-        </ul>
-        <br />
-        <div class="tab-content">
-            <div class="tab-pane active" id="overview">
-                @include('userProfile.userDetails')
-            </div>
-            <div class="tab-pane" id="ratings">
-                @include('userProfile.userRatings')
-            </div>
-            @if(Auth::check() && Auth::id() == $user->id)
-                <!-- <div class="tab-pane" id="messages"> -->
-                    {{-- @include('userProfile.userMessages') --}}
-                <!-- </div> -->
-                <div class="tab-pane" id="settings">
-                    @include('userProfile.userSettings')
+    <div class="row page-content">
+        <div>
+            <ul class="nav nav-tabs" role="tablist" id="tab-list">
+                <li class="active"><a href="#overview" role="tab" data-toggle="tab">Details <i class="fa fa-user"></i></a></li>
+                <li><a href="#ratings" role="tab" data-toggle="tab">Stats <i class="fa fa-bar-chart"></i></a></li>
+                @if(Auth::check() && Auth::id() == $user->id)
+                    <!-- <li><a href="#messages" role="tab" data-toggle="tab">Messages <i class="fa fa-envelope"></i></a></li> -->
+                    <li><a href="#settings" role="tab" data-toggle="tab">Settings <i class="fa fa-cog"></i></a></li>
+                @endif
+            </ul>
+            <br />
+            <div class="tab-content">
+                <div class="tab-pane active" id="overview">
+                    @include('userProfile.userDetails')
                 </div>
-            @endif
+                <div class="tab-pane" id="ratings">
+                    @include('userProfile.userRatings')
+                </div>
+                @if(Auth::check() && Auth::id() == $user->id)
+                    <!-- <div class="tab-pane" id="messages"> -->
+                        {{-- @include('userProfile.userMessages') --}}
+                    <!-- </div> -->
+                    <div class="tab-pane" id="settings">
+                        @include('userProfile.userSettings')
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 @stop
